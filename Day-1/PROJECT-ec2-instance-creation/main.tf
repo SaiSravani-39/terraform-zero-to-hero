874b605fe6ebd1d61d4076548adbb106acc3190e
SaiSravani-39 <141036998+SaiSravani-39@users.noreply.github.com>
2023-12-05 14:42:24 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,9 +6,8 @@ resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
 resource "aws_instance" "this" {
   ami                     = "ami-0dcc1e21636832c5d"
   instance_type           = "m5.large"
-  host_resource_group_arn = "arn:aws:resource-groups:us-west-2:012345678901:group/win-testhost"
-  tenancy                 = "host"
-}
+ }
